83697c3bd6c394b2a1d1f8abcf21648781941391
Khushi-Lalchandani <140065096+Khushi-Lalchandani@users.noreply.github.com>
2025-02-21 16:54:39 +0530

    Updated

--- src/app/add-expense/add-expense.component.ts
+++ src/app/add-expense/add-expense.component.ts
@@ -1,5 +1,5 @@
 import { user } from './../user/user.model';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { IDropdownSettings } from 'ng-multiselect-dropdown/multiselect.model';
 
@@ -10,38 +10,46 @@ import { IDropdownSettings } from 'ng-multiselect-dropdown/multiselect.model';
 })
 export class AddExpenseComponent implements OnInit {
   dropDownList: any[] = [];
-  selectedEmails: string[] = [];
+  currentUserEmail!: string | null;
+  selectedEmails: string[] | any = [];
   dropdownSettings: IDropdownSettings = {};
   billDetails!: FormGroup;
+  percentage1!: number;
+  percentage2!: number;
+  percentage3!: number;
+  percentage4!: number;
+  percentage5!: number;
+
   @Input() options: any;
   @Output() show = new EventEmitter<boolean>();
   @Output() details = new EventEmitter<[]>();
 
   ngOnInit() {
+    this.currentUserEmail = localStorage.getItem('currentUser');
+
     this.options.forEach((option: user) => {
       this.dropDownList = [...this.dropDownList, option.email];
     });
 
+    this.dropdownSettings = {
+      limitSelection: 5,
+      maxHeight: 80,
+      allowSearchFilter: true,
+      searchPlaceholderText: 'Search for email id',
+    };
     this.billDetails = new FormGroup({
-      email: new FormControl('', Validators.required),
+      email: new FormControl([this.currentUserEmail], Validators.required),
       description: new FormControl('', Validators.required),
       amount: new FormControl('', Validators.required),
       splitBill: new FormControl(false),
-      percentage1: new FormControl(null, Validators.required),
-      percentage2: new FormControl(null, Validators.required),
+      percentage: new FormArray([]),
     });
 
     this.billDetails.get('splitBill')?.valueChanges.subscribe((value) => {
       if (value) {
-        this.billDetails.patchValue({
-          percentage1: 50,
-          percentage2: 50,
-        });
-      } else {
-        this.billDetails.patchValue({
-          percentage1: 0,
-          percentage2: 0,
-        });
+        for (let i = 0; i < this.billDetails.value.email.length; i++) {
+          this.billDetails.value.percentage.push(50);
+        }
       }
     });
   }
@@ -49,9 +57,11 @@ export class AddExpenseComponent implements OnInit {
     console.log(this.billDetails.value);
     if (this.billDetails.valid) {
       this.details.emit(this.billDetails.value);
+
       this.onCancel();
     }
   }
+
   onCancel() {
     this.show.emit(false);
   }
@@ -59,82 +69,9 @@ export class AddExpenseComponent implements OnInit {
     console.log($event);
 
     this.selectedEmails = [...this.selectedEmails, $event];
-    console.log(this.selectedEmails);
+    // console.log(this.selectedEmails);
   }
   onDeSelectEmail($event: any) {
     console.log($event);
   }
 }
-// export class AddExpenseComponent implements OnInit {
-//   dropDownList: any[] = [];
-//   selectedEmails: string[] = [];
-//   dropdownSettings: IDropdownSettings = {};
-//   billDetails!: FormGroup;
-//   @Input() options: any;
-//   @Output() show = new EventEmitter<boolean>();
-//   @Output() details = new EventEmitter<[]>();
-
-//   // This will store the dynamically generated percentage form controls
-//   dynamicPercentageControls: FormControl[] = [];
-
-//   ngOnInit() {
-//     this.options.forEach((option: user) => {
-//       this.dropDownList = [...this.dropDownList, option.email];
-//     });
-
-//     this.billDetails = new FormGroup({
-//       email: new FormControl('', Validators.required),
-//       description: new FormControl('', Validators.required),
-//       amount: new FormControl('', Validators.required),
-//       splitBill: new FormControl(false),
-//       // Initially, there will be no percentage fields
-//       percentages: new FormControl([]),
-//     });
-
-//     this.billDetails.get('splitBill')?.valueChanges.subscribe((value) => {
-//       if (value) {
-//         this.billDetails.patchValue({
-//           percentages: [50], // Default to one percentage field for splitting equally
-//         });
-//       } else {
-//         this.billDetails.patchValue({
-//           percentages: [],
-//         });
-//       }
-//     });
-//   }
-
-//   onSelectEmail($event: any) {
-//     this.selectedEmails = [...this.selectedEmails, $event];
-//     this.updatePercentageFields();
-//   }
-
-//   onDeSelectEmail($event: any) {
-//     this.selectedEmails = this.selectedEmails.filter(
-//       (email) => email !== $event
-//     );
-//   }
-
-//   // Dynamically update percentage fields based on the number of selected emails
-//   updatePercentageFields() {
-//     const emailCount = this.selectedEmails.length;
-//     const percentageArray = new Array(emailCount).fill(null);
-
-//     // Set percentage controls for each selected email
-//     this.billDetails.patchValue({
-//       percentages: percentageArray,
-//     });
-//   }
-
-//   onSubmit() {
-//     console.log(this.billDetails.value);
-//     if (this.billDetails.valid) {
-//       this.details.emit(this.billDetails.value);
-//       this.onCancel();
-//     }
-//   }
-
-//   onCancel() {
-//     this.show.emit(false);
-//   }
-// }
